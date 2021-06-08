69281397632124fb76c940f306376d4d8b3aa88b
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-06-08 09:37:44 +0900

    コメント追加

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
+<!-- フッター -->
 </div>
 </body>
 </html>
 <script>
+// どのページか判定した変数を取得し、対応するidをもつclass属性を書き換える
 const div = document.getElementById("<?=$page?>");
 div.classList.add('active');
 </script>
