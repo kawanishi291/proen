c2e6666ad1fb211a2a0518e30407777cf65a8fea
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-06-08 09:37:49 +0900

    コメント追加

--- header.php
+++ header.php
@@ -1,16 +1,20 @@
+<!-- ヘッダー -->
 <!DOCTYPE html>
 <html lang='ja'>
 <head>
     <meta charset='utf-8' />
+    <!-- bootstrap CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <title><?=$title?></title>
+    <!-- fullcalendar CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.5.0/main.min.css">
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.5.0/main.min.js"></script>
 </head>
 <body>
+<!-- ナビゲーションバー -->
 <nav class="navbar navbar-dark mb-5" style="background-color: #008080;">
   <a class="navbar-brand" href="./index.php">認知症予防システム</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,4 +28,5 @@
     </div>
   </div>
 </nav>
+<!-- コンテンツ -->
 <div class="container w-80">
